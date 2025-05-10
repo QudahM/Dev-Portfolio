bf372aca388a91d798b999f457f40aa7983ac04c
Mohammad Qudah <qudahmohammad2002@gmail.com>
2025-05-10 03:53:40 -0400

    fixing the excessive space bug

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -120,7 +120,7 @@ const ContactSection = ({
   };
 
   return (
-    <section className="w-full min-h-screen bg-gray-900 py-20 px-4 relative overflow-hidden">
+    <section className="w-full bg-gray-900 py-20 px-4 relative overflow-hidden">
       {/* Grid pattern */}
       <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]" />
 
